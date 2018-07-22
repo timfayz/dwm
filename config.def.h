1f6f6098e3c0b5f3dd12c74a119986b5417524ff
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2018-07-22 12:43:42 +0200

    Change size for Terminus

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9" };
-static const char dmenufont[]       = "monospace:size=9";
+static const char *fonts[]          = { "monospace:pixelsize=16" };
+static const char dmenufont[]       = "monospace:pixelsize=16";
 static const char normbgcolor[]     = "#0c0c0d"; // normbgcolor #222222
 static const char normfgcolor[]     = "#bebebe"; // normfgcolor #bbbbbb
 static const char normborder[]      = "#0c0c0d"; // normborder #444444
